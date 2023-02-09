1107fa695170afa3e125f3cf9769578d256ac756
Lucas Camargo <lgabrielc22@hotmail.com>
2023-02-09 12:18:16 -0300

    feat: make and record incoming call

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,7 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const AccessToken = require('twilio').jwt.AccessToken;
 const VoiceGrant = AccessToken.VoiceGrant;
 
-const nameGenerator = require('../name_generatorkek');
+const nameGenerator = require('../name_generator');
 const config = require('../config');
 
 let identity;
@@ -38,7 +38,8 @@ exports.voiceResponse = function voiceResponse(requestBody) {
   if (toNumberOrClientName == callerId) {
     const dial = twiml.dial({
       record: 'record-from-answer-dual',
-      recordingStatusCallback: 'https://768b-152-250-74-193.sa.ngrok.io/record'});
+      recordingStatusCallback: 'https://761c-2804-1b3-a9c0-ab25-c575-f8a5-9c39-65a2.sa.ngrok.io/record'});
+    // const dial = twiml.dial();
     // This will connect the caller with your Twilio.Device/client
     dial.client(identity);
   } else if (requestBody.To) {
@@ -46,7 +47,7 @@ exports.voiceResponse = function voiceResponse(requestBody) {
     // set the callerId
     const dial = twiml.dial({callerId,
       record: 'record-from-answer-dual',
-      recordingStatusCallback: 'https://768b-152-250-74-193.sa.ngrok.io/record'});
+      recordingStatusCallback: 'https://761c-2804-1b3-a9c0-ab25-c575-f8a5-9c39-65a2.sa.ngrok.io/record'});
 
     dial.number(toNumberOrClientName);
   } else {
